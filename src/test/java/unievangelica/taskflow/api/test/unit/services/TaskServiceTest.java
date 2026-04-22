64a6edfd559fb7f85c71ead132b089ee21067ad3
Kayran Henrique Reis Teixeira <kayranhenrique2016@gmail.com>
2026-04-22 19:11:41 -0300

    Adição do teste de Criar task sem definir todos atributos

--- src/test/java/unievangelica/taskflow/api/test/unit/services/TaskServiceTest.java
+++ src/test/java/unievangelica/taskflow/api/test/unit/services/TaskServiceTest.java
@@ -2,6 +2,7 @@ package unievangelica.taskflow.api.test.unit.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,6 +11,7 @@ import unievangelica.taskflow.api.domain.persistence.entities.UserEntity;
 import unievangelica.taskflow.api.domain.persistence.repositories.TaskRepository;
 import unievangelica.taskflow.api.domain.persistence.repositories.UserRepository;
 import unievangelica.taskflow.api.domain.service.TaskService;
+import unievangelica.taskflow.api.dto.request.TaskRequestDTO;
 import unievangelica.taskflow.api.dto.response.TaskResponseDTO;
 
 import java.util.List;
@@ -69,8 +71,63 @@ public class TaskServiceTest {
     }
 
     @Test
-    void testarCriarTask() {
+    void testarCriarTaskSemDefinirAtributos() {
+        // arrange
 
+        // mock usuário criador da tarefa
+        UserEntity criador = new UserEntity();
+        criador.setNome("UnitTestServiceUser");
+
+        // mock de request do DTO de criação de usuário com apenas as informações obrigatórias e descrição
+        TaskRequestDTO requestDTO = new TaskRequestDTO(
+                "Unit Test Service Titulo",
+                "Unit Test Service Descrição", // descricao
+                null, // status
+                null, // prioridade
+                null, // prazo
+                null  // idsResponsaveis
+        );
+
+        // retornando a entidade mockada para ser salva pelo .save do método
+        TaskEntity taskSalvaMock = new TaskEntity();
+        taskSalvaMock.setId(10L);
+        taskSalvaMock.setTitulo("Unit Test Service Titulo");
+        taskSalvaMock.setDescricao("Unit Test Service Descrição");
+        taskSalvaMock.setStatus(TaskEntity.Status.pendente);
+        taskSalvaMock.setPrioridade(TaskEntity.Prioridade.media);
+        taskSalvaMock.setCriador(criador);
+        taskSalvaMock.setResponsaveis(List.of());
+
+        // retorna o mock após um entidade for salva
+        when(taskRepository.save(any(TaskEntity.class))).thenReturn(taskSalvaMock);
+
+
+        // act
+        TaskResponseDTO resultado = taskService.criarTask(requestDTO, criador);
+
+
+        // assert
+
+        assertEquals("Unit Test Service Titulo", resultado.titulo());
+        assertEquals("pendente", resultado.status());
+        assertEquals("UnitTestServiceUser", resultado.nomeCriador());
+
+        // captura a entidade que o service montou
+        ArgumentCaptor<TaskEntity> captor = ArgumentCaptor.forClass(TaskEntity.class);
+
+        verify(taskRepository, times(1)).save(captor.capture());
+        TaskEntity entidadeCapturada = captor.getValue();
+
+        // verifica as regras de negócio
+        assertEquals("Unit Test Service Titulo", entidadeCapturada.getTitulo());
+        assertEquals(criador, entidadeCapturada.getCriador());
+        // verifica o if do null para retornar os valores padrão
+        assertEquals(TaskEntity.Prioridade.media, entidadeCapturada.getPrioridade());
+        assertEquals(TaskEntity.Status.pendente, entidadeCapturada.getStatus());
+        assertNull(entidadeCapturada.getPrazo());
+
+        // garante que user repository não seja chamado já que não foi passado os responsáveis
+        verify(userRepository, never()).userFindAllByIds(anyList());
     }
 
 }
