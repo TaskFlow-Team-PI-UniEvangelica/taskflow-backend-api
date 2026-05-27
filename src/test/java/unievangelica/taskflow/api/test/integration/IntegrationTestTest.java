b673d8d9723d58716e8e85f9664568802527b08e
Kayran Henrique Reis Teixeira <kayranhenrique2016@gmail.com>
2026-05-27 14:53:36 -0300

    Criação da lógica do teste de repository atualçização do POM xml e adição de comentário no teste de integração ainda vazio

--- src/test/java/unievangelica/taskflow/api/test/integration/IntegrationTestTest.java
+++ src/test/java/unievangelica/taskflow/api/test/integration/IntegrationTestTest.java
@@ -1,4 +1,12 @@
 package unievangelica.taskflow.api.test.integration;
 
+import org.junit.jupiter.api.Test;
+
 public class IntegrationTestTest {
+
+    @Test
+    void stubIntegrationTest() {
+        // Teste de integração stub (substituir por teste real com Testcontainers quando necessário)
+    }
 }
+
